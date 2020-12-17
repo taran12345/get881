bcf53fe804f4a91230a8e4ba03951ae01021b407
taran12345 <tarangangil26@gmail.com>
2020-12-17 17:20:58 +0530

    added sub method in Calc class from branch br1

---
+++ DemoApp/src/model/Calc.java
@@ -0,0 +1,10 @@
+package model;
+
+public class Calc {
+	public void add(int n1, int n2) {
+		System.out.println("Sum= "+(n1+n2));
+	}
+	public void sub(int n1, int n2) {
+		System.out.println("Difference= "+(n1-n2));
+	} 
+}

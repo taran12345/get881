9e2ebcc4fc74733b9f8b2c6913d335306ae50c9b
Taran gangil <40990378+taran12345@users.noreply.github.com>
2020-12-17 13:39:47 +0530

    Create calculator.java
    Created calculator class and added add method

---
+++ calculator.java
@@ -0,0 +1,9 @@
+class Calculator{
+  public static int add(int n1, int n2){
+    return n1+n2;
+    }
+    
+    public static void main(String []args){
+      system.out.println(add(10,20));
+      }
+    }
